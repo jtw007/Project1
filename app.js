e021322ba97543ac985e248f0f0497aff7dde74d
jtw007 <joshwu095@gmail.com>
2022-12-01 15:24:40 -0800

    base html and css done, some js dom added

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 //---GLOBAL HTML ELEMENTS-----
+tile = document.querySelector('.tile')
+button = document.querySelector('#button')
+
 
 //----DOM EVENT LISTENER-----
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,15 +16,7 @@ const addH1 = () => {
     document.querySelector('body').prepend(h1)
 }
 
-//----Create div elements-----
-const addDiv = () => {
-    for (i = 0; i <42; i++) {
-    const div = document.createElement('div')
-    div.innerText = ' ' 
-    }
-    
-    document.querySelector('#game-container').appendChild(div)
-}
+
 
 
 
@@ -29,3 +24,11 @@ const addDiv = () => {
 
 //--------SCRAPPED CODE/DIDN'T WORK-------------
 // console.log('js testing')
+// const addDiv = () => {
+//     for (i = 0; i <42; i++) {
+//     const div = document.createElement('div')
+//     div.innerText = ' ' 
+//     }
+    
+//     document.querySelector('#game-container').appendChild(div)
+// }
